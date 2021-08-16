e328c19b87e5e4878fbdc9285005fcdb03279d54
Ruben <34570952+thepixelboy@users.noreply.github.com>
2021-08-16 11:03:54 +0200

    feat: converting the database row object to a dictionary before convert it to json

--- main.py
+++ main.py
@@ -24,6 +24,17 @@ class Cafe(db.Model):
     can_take_calls = db.Column(db.Boolean, nullable=False)
     coffee_price = db.Column(db.String(250), nullable=True)
 
+    def to_dict(self):
+        dictionary = {}
+        # Loop through each column in the data record
+        for column in self.__table__.columns:
+            # Create a new dictionary entry;
+            # where the key is the name of the column
+            # and the value is the value of the column
+            dictionary[column.name] = getattr(self, column.name)
+
+        return dictionary
+
 
 @app.route("/")
 def home():
@@ -40,21 +51,7 @@ def get_random_cafe():
     # Return the first record after skipping random_offset rows
     random_cafe = Cafe.query.offset(random_offset).first()
 
-    return jsonify(
-        cafe={
-            "id": random_cafe.id,
-            "name": random_cafe.name,
-            "map_url": random_cafe.map_url,
-            "img_url": random_cafe.img_url,
-            "location": random_cafe.location,
-            "seats": random_cafe.seats,
-            "has_toilet": random_cafe.has_toilet,
-            "has_wifi": random_cafe.has_wifi,
-            "has_sockets": random_cafe.has_sockets,
-            "can_take_calls": random_cafe.can_take_calls,
-            "coffee_price": random_cafe.coffee_price,
-        }
-    )
+    return jsonify(cafe=random_cafe.to_dict())
 
 
 ## HTTP POST - Create Record
