1f9cd3fa4acc7a3fefecedd9a713af1ec5516b1a
Ruben <34570952+thepixelboy@users.noreply.github.com>
2021-08-16 14:10:05 +0200

    feat: update and delete routes

--- main.py
+++ main.py
@@ -82,7 +82,7 @@ def search():
         return jsonify(cafe=cafe.to_dict(), status=200)
     else:
         # Using make_response to allow for status codes (404 Not found here)
-        return make_response(jsonify({"Not found": "Sorry, we don't have a cafe at that location."}), 404)
+        return make_response(jsonify(error={"Not found": "Sorry, we don't have a cafe at that location."}), 404)
 
 
 ## HTTP POST - Create Record
@@ -96,8 +96,44 @@ def add_new_cafe():
 
 
 ## HTTP PUT/PATCH - Update Record
+@app.route("/update-price/<int:cafe_id>", methods=["PATCH"])
+def update_price(cafe_id):
+    new_price = request.args.get("new-price")
+    cafe = db.session.query(Cafe).get(cafe_id)
+
+    if cafe:
+        cafe.coffee_price = new_price
+        db.session.commit()
+
+        return jsonify(response={"success": "Successfully updated the price."})
+    else:
+        return make_response(
+            jsonify(error={"Not Found": "Sorry a cafe with that id was not found in the database."}), 404
+        )
+
 
 ## HTTP DELETE - Delete Record
+@app.route("/report-closed/<int:cafe_id>", methods=["DELETE"])
+def delete_cafe(cafe_id):
+    api_key = "physic-rang-rascal-hydro"
+    requested_api = request.args.get("api-key")
+
+    if api_key == requested_api:
+        cafe = db.session.query(Cafe).get(cafe_id)
+
+        if cafe:
+            db.session.delete(cafe)
+            db.session.commit()
+
+            return make_response(jsonify(response={"success": "Successfully deleted the cafe from the database."}))
+        else:
+            return make_response(
+                jsonify(error={"Not Found": "Sorry a cafe with that id was not found in the database."}), 404
+            )
+    else:
+        return make_response(
+            jsonify(error={"Forbidden": "Sorry, that's not allowed. Make sure you have the correct api_key."}), 403
+        )
 
 
 if __name__ == "__main__":
